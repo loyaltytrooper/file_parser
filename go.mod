9ff62fb5c59ea91eac663f4a59c0c444877a4b36
Rajat Kumar <rajat.kumar@ventura1.com>
2023-03-26 07:55:57 +0530

    pdfcpu starting to give some output

--- go.mod
+++ go.mod
@@ -3,6 +3,32 @@ module file_parser
 go 1.20
 
 require (
+	code.sajari.com/docconv v1.3.5 // indirect
+	github.com/JalfResi/justext v0.0.0-20170829062021-c0282dea7198 // indirect
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/advancedlogic/GoOse v0.0.0-20191112112754-e742535969c1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/araddon/dateparse v0.0.0-20200409225146-d820a6159ab1 // indirect
+	github.com/fatih/set v0.2.1 // indirect
+	github.com/gigawattio/window v0.0.0-20180317192513-0f5467e35573 // indirect
+	github.com/go-resty/resty/v2 v2.3.0 // indirect
+	github.com/hhrutter/lzw v0.0.0-20190829144645-6f07a24e8650 // indirect
+	github.com/hhrutter/tiff v0.0.0-20190829141212-736cae8d0bc7 // indirect
+	github.com/jaytaylor/html2text v0.0.0-20200412013138-3577fbdbcff7 // indirect
 	github.com/ledongthuc/pdf v0.0.0-20220302134840-0c2507a12d80 // indirect
+	github.com/levigross/exp-html v0.0.0-20120902181939-8df60c69a8f5 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/olekukonko/tablewriter v0.0.4 // indirect
+	github.com/otiai10/gosseract/v2 v2.2.4 // indirect
 	github.com/pdfcpu/pdfcpu v0.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/richardlehane/mscfb v1.0.3 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
+	golang.org/x/image v0.5.0 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,126 +1,126 @@
 package main
 
 import (
-	"file_parser/models"
 	"fmt"
-	"github.com/ledongthuc/pdf"
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
 	"strconv"
 	"strings"
-	"time"
 )
 
 func main() {
-	str, err := readPdf("./Jan.pdf")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(str)
+	//str, err :=
+	readPdf("./feb.pdf")
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//fmt.Println(str)
 }
 
-func readPdf(path string) (string, error) {
-	//reachedStartPoint := false
-	f, r, err := pdf.Open(path)
-	defer func() {
-		_ = f.Close()
-	}()
-	if err != nil {
-		return "", err
-	}
-	totalPage := r.NumPage()
-
-	var transactions models.Transactions
-
-	for pageIndex := 1; pageIndex <= totalPage; pageIndex++ {
-		p := r.Page(pageIndex)
-		if p.V.IsNull() {
-			continue
-		}
-
-		var currentBalance float64
-		var prevBalance float64
-		//var prevDescriptionY float64
-		//var prevDescriptionX float64
-		rows, _ := p.GetTextByRow()
-		for _, row := range rows {
-			transactionTime, err := time.Parse("02-Jan-2006", row.Content[0].S)
-			if err != nil {
-				//if row.Content.Len() == 1 {
-				//	if math.Abs(prevDescriptionY-row.Content[0].Y) < 11 {
-				//		transactions.Txns[len(transactions.Txns)-1].Description += row.Content[0].S
-				//	}
-				//}
-				continue
-			} else {
-				currentBalance, err = BeautifyCommaNumber(row.Content[row.Content.Len()-1].S)
-				if err != nil {
-					fmt.Println(err)
-				} else {
-					if row.Content.Len() == 3 {
-						txnAmount, err := BeautifyCommaNumber(row.Content[row.Content.Len()-2].S)
-						if err != nil {
-							//prevDescriptionY = row.Content[row.Content.Len()-2].Y
-							//prevDescriptionX = row.Content[row.Content.Len()-2].X
-							transactions.Txns = append(transactions.Txns, models.Transaction{
-								Date:        transactionTime,
-								Description: row.Content[row.Content.Len()-2].S,
-								Credit:      0,
-								Debit:       0,
-								FinalAmount: currentBalance,
-							})
-						} else if prevBalance < currentBalance {
-							transactions.Txns = append(transactions.Txns, models.Transaction{
-								Date:        transactionTime,
-								Description: "",
-								Credit:      txnAmount,
-								Debit:       0,
-								FinalAmount: currentBalance,
-							})
-						} else {
-							transactions.Txns = append(transactions.Txns, models.Transaction{
-								Date:        transactionTime,
-								Description: "",
-								Credit:      0,
-								Debit:       txnAmount,
-								FinalAmount: currentBalance,
-							})
-						}
-					} else if row.Content.Len() == 4 {
-						txnAmount, err := BeautifyCommaNumber(row.Content[row.Content.Len()-2].S)
-						if err != nil {
-							fmt.Println(err)
-						} else if prevBalance < currentBalance {
-							//prevDescriptionY = row.Content[row.Content.Len()-3].Y
-							//prevDescriptionX = row.Content[row.Content.Len()-3].X
-							transactions.Txns = append(transactions.Txns, models.Transaction{
-								Date:        transactionTime,
-								Description: row.Content[row.Content.Len()-3].S,
-								Credit:      txnAmount,
-								Debit:       0,
-								FinalAmount: currentBalance,
-							})
-						} else if prevBalance > currentBalance {
-							fmt.Println("Debit")
-							//prevDescriptionY = row.Content[row.Content.Len()-3].Y
-							//prevDescriptionX = row.Content[row.Content.Len()-3].X
-							transactions.Txns = append(transactions.Txns, models.Transaction{
-								Date:        transactionTime,
-								Description: row.Content[row.Content.Len()-3].S,
-								Credit:      0,
-								Debit:       txnAmount,
-								FinalAmount: currentBalance,
-							})
-						}
-					}
-					prevBalance = currentBalance
-				}
-			}
-		}
-	}
-	for _, x := range transactions.Txns {
-		fmt.Println(x)
-	}
-	return "", nil
-}
+//func readPdf(path string) (string, error) {
+//	//reachedStartPoint := false
+//	f, r, err := pdf.Open(path)
+//	defer func() {
+//		_ = f.Close()
+//	}()
+//	if err != nil {
+//		return "", err
+//	}
+//	totalPage := r.NumPage()
+//
+//	var transactions models.Transactions
+//
+//	for pageIndex := 1; pageIndex <= totalPage; pageIndex++ {
+//		p := r.Page(pageIndex)
+//		if p.V.IsNull() {
+//			continue
+//		}
+//
+//		var currentBalance float64
+//		var prevBalance float64
+//		//var prevDescriptionY float64
+//		//var prevDescriptionX float64
+//		rows, _ := p.GetTextByRow()
+//		for _, row := range rows {
+//			transactionTime, err := time.Parse("02-Jan-2006", row.Content[0].S)
+//			if err != nil {
+//				//if row.Content.Len() == 1 {
+//				//	if math.Abs(prevDescriptionY-row.Content[0].Y) < 11 {
+//				//		transactions.Txns[len(transactions.Txns)-1].Description += row.Content[0].S
+//				//	}
+//				//}
+//				continue
+//			} else {
+//				currentBalance, err = BeautifyCommaNumber(row.Content[row.Content.Len()-1].S)
+//				if err != nil {
+//					fmt.Println(err)
+//				} else {
+//					if row.Content.Len() == 3 {
+//						txnAmount, err := BeautifyCommaNumber(row.Content[row.Content.Len()-2].S)
+//						if err != nil {
+//							//prevDescriptionY = row.Content[row.Content.Len()-2].Y
+//							//prevDescriptionX = row.Content[row.Content.Len()-2].X
+//							transactions.Txns = append(transactions.Txns, models.Transaction{
+//								Date:        transactionTime,
+//								Description: row.Content[row.Content.Len()-2].S,
+//								Credit:      0,
+//								Debit:       0,
+//								FinalAmount: currentBalance,
+//							})
+//						} else if prevBalance < currentBalance {
+//							transactions.Txns = append(transactions.Txns, models.Transaction{
+//								Date:        transactionTime,
+//								Description: "",
+//								Credit:      txnAmount,
+//								Debit:       0,
+//								FinalAmount: currentBalance,
+//							})
+//						} else {
+//							transactions.Txns = append(transactions.Txns, models.Transaction{
+//								Date:        transactionTime,
+//								Description: "",
+//								Credit:      0,
+//								Debit:       txnAmount,
+//								FinalAmount: currentBalance,
+//							})
+//						}
+//					} else if row.Content.Len() == 4 {
+//						txnAmount, err := BeautifyCommaNumber(row.Content[row.Content.Len()-2].S)
+//						if err != nil {
+//							fmt.Println(err)
+//						} else if prevBalance < currentBalance {
+//							//prevDescriptionY = row.Content[row.Content.Len()-3].Y
+//							//prevDescriptionX = row.Content[row.Content.Len()-3].X
+//							transactions.Txns = append(transactions.Txns, models.Transaction{
+//								Date:        transactionTime,
+//								Description: row.Content[row.Content.Len()-3].S,
+//								Credit:      txnAmount,
+//								Debit:       0,
+//								FinalAmount: currentBalance,
+//							})
+//						} else if prevBalance > currentBalance {
+//							fmt.Println("Debit")
+//							//prevDescriptionY = row.Content[row.Content.Len()-3].Y
+//							//prevDescriptionX = row.Content[row.Content.Len()-3].X
+//							transactions.Txns = append(transactions.Txns, models.Transaction{
+//								Date:        transactionTime,
+//								Description: row.Content[row.Content.Len()-3].S,
+//								Credit:      0,
+//								Debit:       txnAmount,
+//								FinalAmount: currentBalance,
+//							})
+//						}
+//					}
+//					prevBalance = currentBalance
+//				}
+//			}
+//		}
+//	}
+//	for _, x := range transactions.Txns {
+//		fmt.Println(x)
+//	}
+//	return "", nil
+//}
 
 func BeautifyCommaNumber(str string) (float64, error) {
 	txnArr := strings.Split(str, ",")
@@ -129,4 +129,23 @@ func BeautifyCommaNumber(str string) (float64, error) {
 		txn += str
 	}
 	return strconv.ParseFloat(txn, 64)
+}
+
+func readPdf(fileName string) {
+	//err := api.DecryptFile(fileName, "act_feb", model.NewAESConfiguration("RAJA2712", "RAJA2712", 128))
+	//if err != nil {
+	//	fmt.Println(err.Error())
+	//}
+
+	err := api.ExtractContentFile("feb.pdf", "act_feb", nil, model.NewAESConfiguration("RAJA2712", "RAJA2712", 128))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	//fmt.Println(ctx.Title)
+	//fmt.Println(ctx.Valid)
+	//fmt.Println(ctx.CreationDate)
+	//fmt.Println(ctx.Pages())
+	//fmt.Println(ctx.Keywords)
+	//fmt.Println(ctx.OwnerPWNew)
+	//fmt.Println(ctx.XRefTable.CurPage)
 }
